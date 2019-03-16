61270cb953846cf00dacf08fd8d0938d5521c4a6
tiff-wang <wangtiffany@gmail.com>
2019-03-16 10:52:00 -0400

    integrated static to react

--- react/public/components/home.jsx
+++ react/public/components/home.jsx
@@ -4,23 +4,24 @@ class HomePage extends React.Component {
 
     render() {
         return(
-            <div id="home">
-            <iframe width="640" height="360" class="video-parallax"
+            <div className="intro">
+            <iframe width="640" height="360" className="video-parallax"
                     src="https://www.youtube.com/embed/Xd0Ok-MkqoE?autoplay=1&loop=1&mute=1&rel=1?autoreplay=1" 
                     frameborder="0" allow="autoplay; encrypted-media" 
-                    allowfullscreen>
+                    allowFullScreen>
             </iframe>
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-10 col-md-offset-1">
-                        <h1 class="brand-heading">Here to find your dream car? </h1>
-                        <p class="intro-text">
-                            360.Agency
-                        </p>
-                        <a href="#about" class="page-scroll" id="intro-button">
-                            Let's get started
-                        </a>
+            <div className="intro-body">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-10 col-md-offset-1">
+                            <h1 className="brand-heading">Here to find your dream car? </h1>
+                            <p className="intro-text">
+                                360.Agency
+                            </p>
+                            <a href="#about" className="page-scroll" id="intro-button">
+                                Let's get started
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>

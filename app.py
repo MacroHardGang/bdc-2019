7d32e55410ed7dbd2ceddb9a68836fbc433b1ed3
tiff-wang <wangtiffany@gmail.com>
2019-03-16 14:22:02 -0400

    deleted react and changed front end

--- app.py
+++ app.py
@@ -3,40 +3,40 @@ import flask
 import json
 import pandas as pd
 
-from data_fetchers import DataFetcher
+# from data_fetchers import DataFetcher
 from flask import (
     Flask,
     jsonify,
     request,
     render_template
 )
-from flask_cors import CORS
-from sklearn.cluster import MeanShift
-from IPython import embed
+# from flask_cors import CORS
+# from sklearn.cluster import MeanShift
+# from IPython import embed
 
 
-class MyJSONEncoder(flask.json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, decimal.Decimal):
-            # Convert decimal instances to strings.
-            return str(obj)
-        return super(MyJSONEncoder, self).default(obj)
+# class MyJSONEncoder(flask.json.JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, decimal.Decimal):
+#             # Convert decimal instances to strings.
+#             return str(obj)
+#         return super(MyJSONEncoder, self).default(obj)
 
 
-# Setup app
+# # Setup app
 app = Flask(__name__, template_folder='templates')
-app.json_encoder = MyJSONEncoder
-CORS(app)
+# app.json_encoder = MyJSONEncoder
+# CORS(app)
 
-# Setup data fetcher
-# TODO: Move to config file
-data_fetcher = DataFetcher(
-    host="hackathon-db.bdc.n360.io",
-    user="events",
-    passwd="Hack@th0n2019",
-    port=3306,
-    db="hackathon",
-)
+# # Setup data fetcher
+# # TODO: Move to config file
+# data_fetcher = DataFetcher(
+#     host="hackathon-db.bdc.n360.io",
+#     user="events",
+#     passwd="Hack@th0n2019",
+#     port=3306,
+#     db="hackathon",
+# )
 
 
 @app.route("/")
@@ -46,21 +46,21 @@ def index():
     '''
     return render_template('index.html')
 
-def MSCluster(df):
-    X = pd.as_matrix(df)
-    k1 = MeanShift().fit(X=X)
-    return k1.predict(X)
+# def MSCluster(df):
+#     X = pd.as_matrix(df)
+#     k1 = MeanShift().fit(X=X)
+#     return k1.predict(X)
 
-@app.route("/requirements", methods=['POST'])
-def requirements():
-    data = json.loads(request.data)
-    inventory = data_fetcher.get_car_inventory(price_low=data['low'], price_high=data['high'])
+# @app.route("/requirements", methods=['POST'])
+# def requirements():
+#     data = json.loads(request.data)
+#     inventory = data_fetcher.get_car_inventory(price_low=data['low'], price_high=data['high'])
 
-    # ML code goes here
-    clu_labels = MSCluster(inventory)
+#     # ML code goes here
+#     clu_labels = MSCluster(inventory)
 
 
-    return jsonify(inventory)
+#     return jsonify(inventory)
 
 
 if __name__ == '__main__':
--- react/README.md
+++
@@ -1,3 +0,0 @@
-# mcgillai-internal-cms
-
-The repository holds the code for the McGill AI Society internal content management system

28cf5a4e0794d5664d4364d0382a618ded104609
tiff-wang <wangtiffany@gmail.com>
2019-03-16 10:40:32 -0400

    ui changes

--- react/public/components/home.jsx
+++ react/public/components/home.jsx
@@ -4,7 +4,27 @@ class HomePage extends React.Component {
 
     render() {
         return(
-            <h1>Home Page</h1>
+            <div id="home">
+            <iframe width="640" height="360" class="video-parallax"
+                    src="https://www.youtube.com/embed/Xd0Ok-MkqoE?autoplay=1&loop=1&mute=1&rel=1?autoreplay=1" 
+                    frameborder="0" allow="autoplay; encrypted-media" 
+                    allowfullscreen>
+            </iframe>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        <h1 class="brand-heading">Here to find your dream car? </h1>
+                        <p class="intro-text">
+                            360.Agency
+                        </p>
+                        <a href="#about" class="page-scroll" id="intro-button">
+                            Let's get started
+                        </a>
+                    </div>
+                </div>
+            </div>
+            </div>
         )
     }
 

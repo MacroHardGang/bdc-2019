3b4ee2dc85258c98ad4b8e874ae3f466e7af343e
nabilchowdhury <nabillionaire@live.com>
2019-03-16 09:54:04 -0400

    Adding IPython debugging. use embed() to set breakpoints in code

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+import requests
+
+from flask import (
+    Flask,
+    request
+)
+
+from IPython import embed
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return ""
+
+@app.route("/requirements", methods=['POST'])
+def requirements():
+    embed()
+
 
 if __name__ == '__main__':
      app.run(port=5000)

1b4c2b557c40b4a0b771627c3937704a427657ad
nabilchowdhury <nabillionaire@live.com>
2019-03-16 11:00:17 -0400

    Adding mysql connection

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-import data_fetchers
+from data_fetchers import DataFetcher
+from flask import Flask
+from flask_cors import CORS
+from IPython import embed
 
-from flask import (
-    Flask
+
+# Setup app
+app = Flask(__name__)
+CORS(app)
+
+# Setup data fetcher
+# TODO: Move to config file
+data_fetcher = DataFetcher(
+    host="hackathon-db.bdc.n360.io",
+    user="events",
+    passwd="Hack@th0n2019",
+    port=3306,
+    db="hackathon",
 )
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
+    '''
+    Landing page
+    TODO: route to landing page for dev, static page for prod
+    '''
     return ""
+
 
 @app.route("/requirements", methods=['POST'])
 def requirements():
-    inventory = data_fetchers.get_car_inventory()
+    # Mock
+    price = {
+        'low': 13000,
+        'high': 33000
+    }
+
+    car_type = 'blah'
+    inventory = data_fetcher.get_car_inventory(price_low=price['low'], price_high=price['high'])
     return inventory
 
 

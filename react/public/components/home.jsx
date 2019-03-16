2e12ef3e5d87fa006cf56b34a2bb09cdbba6b4f7
tiff-wang <wangtiffany@gmail.com>
2019-03-16 11:21:33 -0400

    with imported css

--- react/public/components/home.jsx
+++ react/public/components/home.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
+import './home.css'
+
+var iframe = '<iframe src="https://www.youtube.com/embed/Xd0Ok-MkqoE?autoplay=1&loop=1&mute=1&rel=1?autoreplay=1" width="540" height="450" allowFullScreen allow="autoplay; encrypted-media></iframe>'; 
 
 class HomePage extends React.Component {
 
     render() {
         return(
+            <div id="intro">
+
+            <div dangerouslySetInnerHTML={ this.iframe() } />
+
             <div className="intro">
-            <iframe width="640" height="360" className="video-parallax"
-                    src="https://www.youtube.com/embed/Xd0Ok-MkqoE?autoplay=1&loop=1&mute=1&rel=1?autoreplay=1" 
-                    frameborder="0" allow="autoplay; encrypted-media" 
-                    allowFullScreen>
-            </iframe>
-            <div className="intro-body">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-10 col-md-offset-1">
-                            <h1 className="brand-heading">Here to find your dream car? </h1>
-                            <p className="intro-text">
-                                360.Agency
-                            </p>
-                            <a href="#about" className="page-scroll" id="intro-button">
-                                Let's get started
-                            </a>
+                <div className="intro-body">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-10 col-md-offset-1">
+                                <h1 className="brand-heading">Here to find your dream car? </h1>
+                                <p className="intro-text">
+                                    360.Agency
+                                </p>
+                                <a href="#about" className="page-scroll intro-button">
+                                    Let's get started
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -28,7 +31,6 @@ class HomePage extends React.Component {
             </div>
         )
     }
-
 }
 
 export default HomePage
